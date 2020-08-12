408fd888716a4b68aff31a5bdbd00807f4e9ce99
Alex Ochigov <alex.ochigov@gmail.com>
2020-08-12 12:57:00 +0300

    setup project

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Alex Ochigov on 8/12/20.
+//  Copyright © 2020 Alex Ochigov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
